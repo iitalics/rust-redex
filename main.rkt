d0cd0e7ab1da7e58d95e55153f5ad642c88d9faf
Milo Turner <iitalics@gmail.com>
2017-12-30 18:23:20 -0500

    Types and typechecking l-values

--- main.rkt
+++ main.rkt
@@ -17,7 +17,7 @@
      (let ℓ ([x e]) e)
      (new e)
      lv
-     (ref q ℓ lv)]
+     (ref ℓ q lv)]
 
   ; paths
   [p ::= hole (* p)]
@@ -28,3 +28,40 @@
 
 (define lv?
   (redex-match? Rust lv))
+
+
+;; ------------------------------------------------------------
+;; types
+
+(define-extended-language Rust+T Rust
+  ; types
+  [τ ::=
+     Unit
+     Integer
+     [Ref ℓ q τ]
+     [Ptr τ]]
+
+  ; type context
+  [Γ ::= ([x τ] ...)])
+
+(define-judgment-form Rust+T
+  ;; typecheck l-values
+  #:mode (⊢lv I I O)
+  #:contract (⊢lv Γ lv τ)
+
+  [(where [τ] (find x Γ))
+   ------ "TL-Var"
+   (⊢lv Γ x τ)]
+
+  [(⊢lv Γ lv [Ref _ _ τ])
+   ------ "TL-Ref"
+   (⊢lv Γ (* lv) τ)]
+
+  [(⊢lv Γ lv [Ptr τ])
+   ------ "TL-Ptr"
+   (⊢lv Γ (* lv) τ)])
+
+(module+ test
+  (test-judgment-holds (⊢lv ([x (Ptr Integer)]) x (Ptr Integer)))
+  (test-judgment-holds (⊢lv ([x (Ptr Integer)]) (* x) Integer))
+  (test-judgment-holds (⊢lv ([x (Ref ℓ1 imm Unit)]) (* x) Unit)))
